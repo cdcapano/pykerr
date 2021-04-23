e24a42d026669e630a4e204370ad074e89e66bc9
Collin Capano <collin.capano@aei.mpg.de>
2021-04-23 20:18:27 +0000

    fix typo in add norms

--- scripts/add_norms.sh
+++ scripts/add_norms.sh
@@ -2,5 +2,5 @@
 set -e
 for ll in 2 3 4 5 6 7; do
     mode=l${ll}
-    nohup python tabulate_norms.py -i ../pykerr/data/${mode}.hdf  -s 2 &> tabnorm-${mode}.out &
+    nohup python tabulate_norms.py --input-file ../pykerr/data/${mode}.hdf  -s 2 &> tabnorm-${mode}.out &
 done
