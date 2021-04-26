a3a453d74016ce3f9c399a2e40b605ceffbf4ef4
Collin Capano <collin.capano@aei.mpg.de>
2021-04-26 14:13:51 +0000

    thin the data by 5

--- scripts/get_files.sh
+++ scripts/get_files.sh
@@ -6,5 +6,5 @@ for ll in 2 3 4 5 6 7; do
     mkdir -p ${mode}
     wget http://blackholes.ist.utl.pt/Webpagecodes/${mode}.tar.gz
     tar -xzvf ${mode}.tar.gz -C ${mode}
-    python convert_to_hdf.py -i ${mode} -o ../pykerr/data/${mode}.hdf
+    python convert_to_hdf.py --thin 5 -i ${mode} -o ../pykerr/data/${mode}.hdf
 done
